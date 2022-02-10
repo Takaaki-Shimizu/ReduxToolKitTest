99b28719639d7505cee9aab831b2a8fdd4d1db15
Takaaki-Shimizu <tattaka_feelmysoul@yahoo.co.jp>
2022-02-10 16:29:36 +0900

    Initialize repository

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import RenderInput from "./RenderInput";
+import FrameworkList from "./FrameworkList";
+import UseEffectRender from "./UseEffectRender";
+import MockServer from "./MockServer";
+import Redux from "./Redux";
+import ReduxAsync from "./ReduxAsync";
 
 function App() {
+  const output = (text) => {
+    console.log(text);
+  };
+  const data = [
+    {
+      id: 1,
+      item: "React",
+    },
+    {
+      id: 2,
+      item: "Angular",
+    },
+    {
+      id: 3,
+      item: "Vue",
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        <RenderInput outputConsole={output} />
+        <FrameworkList frameworks={data} />
+        {/* <FrameworkList /> */}
+        <UseEffectRender />
+        <MockServer />
+        <Redux />
+        <ReduxAsync />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/FrameworkList.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const FrameworkList = (props) => {
+  const { frameworks } = props;
+  if (!frameworks || !frameworks.length) {
+    return <h1>No data</h1>;
+  }
+  return (
+    <div>
+      <ul>
+        {frameworks.map(({ id, item }) => (
+          <li key={id}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FrameworkList;
